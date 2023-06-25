50b1cd21cddb3f532742d1eea9cca83c657d1965
Adzhimambetov Arsen <adzhimambetov.a.i20@gmail.com>
2023-06-25 18:04:55 +0300

    driving-update

--- js/address.js
+++ js/address.js
@@ -1,4 +1,25 @@
-// const card = document.querySelector('.address__item');
-// card.addEventListener('click', function () {
-//   card.classList.add('.address__item--active');
-// });
+const cardsContainer = document.querySelector('.address__list');
+const cards = document.querySelectorAll('.address__item');
+
+let isMobile = window.matchMedia('(max-width: 480px)').matches;
+let cardWidth = isMobile ? cards[0].offsetWidth + 10 : cards[0].offsetWidth + 20;
+
+cardsContainer.scrollLeft = (cardsContainer.scrollWidth - cardsContainer.offsetWidth) / 2;
+
+cardsContainer.addEventListener('scroll', () => {
+  let scrollLeft = cardsContainer.scrollLeft;
+  let centerCardIndex = Math.round(scrollLeft / cardWidth);
+
+  if (isMobile) {
+    centerCardIndex += 1;
+  }
+
+  let centerCard = cards[centerCardIndex];
+
+  if (centerCard) {
+    let centerCardPosition = centerCard.offsetLeft + centerCard.offsetWidth / 2;
+    let containerCenter = cardsContainer.offsetWidth / 2;
+
+    cardsContainer.scrollLeft += centerCardPosition - containerCenter;
+  }
+});
